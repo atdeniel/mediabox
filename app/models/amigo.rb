7fb51223a8aa58b350e639af434e53fcc8557f53
Daniel Moros <jdanielm90@hotmail.com>
2013-10-20 18:04:20 -0430

    6to Commit
    -Se agregaron mas metodos generales a los modelos Amigo y User

--- app/models/amigo.rb
+++ app/models/amigo.rb
@@ -1,15 +1,27 @@
 class Amigo < ActiveRecord::Base
 
-	attr_reader :usuarioSolicitaPeticion , :usuarioRecibePeticion
+	/attr_reader :usuarioSolicitaPeticion , :usuarioRecibePeticion/
 
 	def nuevaAmistad(usuarioSolicitaPeticion,usuarioRecibePeticion,fechaPeticion)
-		nuevaAmistad = Amigo.new fk_usuario1:usuarioSolicitaPeticion, fk_usuario2:usuarioRecibePeticion, status:"pendiente", 
+		nuevaAmistad = Amigo.new fk_usuario1:usuarioSolicitaPeticion, fk_usuario2:usuarioRecibePeticion, status:"pendiente",
 		               fecha:fechaPeticion, leido:"no"
      	nuevaAmistad.new_record?
 		nuevaAmistad.save
 	end
 
+	def eliminarAmistad(usuarioSolicitaPeticion,usuarioRecibePeticion)
+		amistad = Amigo.find_by(fk_usuario1: usuarioSolicitaPeticion, fk_usuario2: usuarioRecibePeticion)
+        amistad.destroy
+    end
 
+	def cambiarStatusParaAmigo(usuarioSolicitaPeticion,usuarioRecibePeticion)
+		amistad = Amigo.find_by(fk_usuario1: usuarioSolicitaPeticion, fk_usuario2: usuarioRecibePeticion)
+		amistad.status = "amigo"
+	end
 
+	def cambiarLeido(usuarioSolicitaPeticion,usuarioRecibePeticion)
+		amistad = Amigo.find_by(fk_usuario1: usuarioSolicitaPeticion, fk_usuario2: usuarioRecibePeticion)
+		amistad.leido="si"
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,64 @@
 class User < ActiveRecord::Base
-  def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.location = auth.info.location
-      user.description = auth.info.description
-      user.image = auth.info.image
-      user.phone = auth.info.phone
-      user.email = auth.info.email
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
-    end
-  end
+
+	def self.from_omniauth(auth)
+		where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+			user.provider = auth.provider
+			user.uid = auth.uid
+			user.name = auth.info.name
+			user.first_name = auth.info.first_name
+			user.last_name = auth.info.last_name
+			user.location = auth.info.location
+			user.description = auth.info.description
+			user.image = auth.info.image
+			user.phone = auth.info.phone
+			user.email = auth.info.email
+			user.oauth_token = auth.credentials.token
+			user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+			user.save!
+		end
+	end
+
+	def modificarNombreUsuario(nuevoNombre,nuevoApellido,idUsuario)
+		usuario = User.find(idUsuario)
+		usuario.first_name=nuevoNombre
+		usuario.last_name=nuevoApellido
+		usuario.save
+	end
+
+	def modificarLugar(nuevoLugar,idUsuario)
+		usuario = User.find(idUsuario)
+		usuario.lugar = nuevoLugar
+		usuario.save
+	end
+
+	def modificarDescripcion (nuevaDescripcion, idUsuario)
+		usuario = User.find(idUsuario)
+		usuario.description = nuevaDescripcion
+		usuario.save
+	end	
+
+	def modificarFoto (nuevaFoto, idUsuario)
+		usuario = User.find(idUsuario)
+		usuario.image = nuevaFoto
+		usuario.save
+	end
+
+	def modificarEmail (nuevoEmail, idUsuario)
+		usuario = User.find(idUsuario)
+		usuario.email = nuevoEmail
+		usuario.save
+	end
+
+	def modificarTelefono (nuevoTelefono, idUsuario)
+		usuario = User.find(idUsuario)
+		usuario.phone = nuevoTelefono
+		usuario.save
+	end
+
+	def eliminarUsuario (idUsuario)
+		usuario = Usuario.find(idUsuario)
+		usuario.destroy
+	end
+
+
 end
