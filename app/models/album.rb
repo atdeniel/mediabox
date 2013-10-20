4986cd0209bbf7c9dc82c326ce06be4c78f9f02a
Daniel Moros <jdanielm90@hotmail.com>
2013-10-19 20:04:41 -0430

    4to Commit
    -Se agregaron los metodos en general de Album (crear, consultar y
    modificar)
    -Solo se agrego el metodo nuevaAmistad (mandar/crear una peticion a otro
    usuario) de Amigo
    -Cuando hace login con Facebook o Google, no aparece 2 Sign out

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,76 @@
 class Album < ActiveRecord::Base
+
+	attr_reader :idAlbum, :idUsuario
+
+	def crearNuevoAlbum (nombreAlbum,fechaCreacionAlbum,descripcionAlbum,lugarAlbum,imagenAlbum,privacidadAlbum,idUsuario)
+		nuevoAlbum = Album.new nombre:nombreAlbum, fecha:fechaCreacionAlbum, descripcion:descripcionAlbum, lugar:lugarAlbum, 
+		             imagen:imagenAlbum, privacidad:privacidadAlbum, fk_usuario:idUsuario
+		nuevoAlbum.new_record?
+		nuevoAlbum.save
+	end
+
+	def iniciarAlbum (idUsuario, idAlbum)
+		@idAlbum, @idUsuario = idAlbum, idUsuario
+	end
+
+	def eliminarAlbum
+		album = Album.find(idAlbum)
+		album.destroy
+	end
+
+	def nombreAlbum
+		nombre = Album.find(idAlbum).nombre
+		return nombre 
+	end
+
+	def fechaCreacionAlbum
+	    fecha = Album.find(idAlbum).fecha
+	    return fecha	
+	end
+
+	def descripcionAlbum
+	    descripcion = Album.find(idAlbum).descripcion
+		return descripcion
+	end
+
+	def lugarAlbum
+	    lugar = Album.find(idAlbum).lugar		
+		return lugar
+	end
+
+	def imagenAlbum
+		imagen = Album.find(idAlbum).imagen
+		return imagen
+	end
+
+	def usuarioAlbum
+		usuario = Album.find(idAlbum).fk_usuario
+		return usuario
+	end	
+
+	def modificarNombreAlbum (nuevoNombreAlbum)
+		album = Album.find(idAlbum)
+		album.nombre = nuevoNombreAlbum
+		album.save
+	end
+
+	def modificarDescripcionAlbum (nuevaDescripcionAlbum)
+		album = Album.find(idAlbum)
+		album.descripcion = nuevaDescripcionAlbum
+		album.save
+	end
+
+	def modificarLugarAlbum (nuevolugarAlbum)
+		album = Album.find(idAlbum)
+		album.lugar = nuevolugarAlbum
+		album.save
+	end
+
+	def modificarPrivacidadAlbum (nuevaPrivacidadAlbum)
+		album = Album.find(idAlbum)
+		album.privacidad = nuevaPrivacidadAlbum
+		album.save
+	end
+
+
 end
--- app/models/amigo.rb
+++ app/models/amigo.rb
@@ -1,2 +1,15 @@
 class Amigo < ActiveRecord::Base
+
+	attr_reader :usuarioSolicitaPeticion , :usuarioRecibePeticion
+
+	def nuevaAmistad(usuarioSolicitaPeticion,usuarioRecibePeticion,fechaPeticion)
+		nuevaAmistad = Amigo.new fk_usuario1:usuarioSolicitaPeticion, fk_usuario2:usuarioRecibePeticion, status:"pendiente", 
+		               fecha:fechaPeticion, leido:"no"
+     	nuevaAmistad.new_record?
+		nuevaAmistad.save
+	end
+
+
+
+
 end
