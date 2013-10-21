cf11c72f6e554124fc041200ebf455a05980681c
Daniel Moros <jdanielm90@hotmail.com>
2013-10-21 12:45:42 -0430

    8vo Commit
    -Se agrego login con Facebook y Google+ en la pagina, principal
    -Muestra la pestaña perfil cuando esta logeado
    -Carga los datos obtenidos por la autenticacion en el perfil

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Project::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#profile'
+  root 'sessions#index'
+  get "sessions/profile"
   get "sessions/index"
 
   # Example of regular route:
