e2221d270fa25750e7bbfe94e5e5f2ac6000e5ca
Daniel Moros <jdanielm90@hotmail.com>
2013-10-27 15:47:20 -0430

    Bug usuario
    -Bug arreglado cuando hacias log out de Google+, y volvias hacer Log in,
    se traia otra vez todo los datos de Google+

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
 		where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+
+			validacionUsuario = User.find_by(uid:auth.uid)
+			if !(validacionUsuario)
+
 			user.provider = auth.provider
 			user.uid = auth.uid
 			user.name = auth.info.name
@@ -16,6 +20,8 @@ class User < ActiveRecord::Base
 			user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 			user.privacity = 'privado'
 			user.save!
+
+		end
 		end
 	end
 
