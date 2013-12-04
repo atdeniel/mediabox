29fbf312a834f8a1d236a159df1c816efa55ef3d
Luz M. Eljuri <lucesita472@gmail.com>
2013-12-03 21:53:43 -0430

    Commit 9 - Logs
    Edicion del log, se ve mejor que antes, aun falta modificar unas cositas
    pero estoy revisando, esto es para tenerlo de respaldo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Project::Application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::Logger.new('your_app.log')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
